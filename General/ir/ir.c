71f5e00ecae5536af4ca67c506918e596a95f54a
Oleg Pudeyev <oleg@bsdpower.com>
2015-12-24 04:34:50 -0500

    Apply FreeBSD patch patch-General_ir_ir.c

--- General/ir/ir.c
+++ General/ir/ir.c
@@ -16,7 +16,7 @@
 #include "ir.h"
 
 /* Important stuff to know */
-static gboolean keepGoing = FALSE;
+static volatile gboolean keepGoing = FALSE;
 
 /* The thread handle */
 static pthread_t irapp_thread;
