d7412dc25667f4b29c634b864c435907491d3f79
Oleg Pudeyev <oleg@bsdpower.com>
2015-12-24 04:34:50 -0500

    Apply FreeBSD patch patch-Input_mikmod_drv__xmms.c

--- Input/mikmod/drv_xmms.c
+++ Input/mikmod/drv_xmms.c
@@ -117,6 +117,10 @@ static BOOL xmms_Reset(void)
 	return VC_Init();
 }
 
+static void xmms_CommandLine(CHAR * commandLine)
+{
+}
+
 MDRIVER drv_xmms =
 {
 	NULL,
@@ -127,6 +131,7 @@ MDRIVER drv_xmms =
         "xmms",
         NULL,
 #endif
+        xmms_CommandLine, 
         xmms_IsThere, 
 	VC_SampleLoad,
 	VC_SampleUnload,
