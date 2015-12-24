c58b77a83887c9d04f0f084e6d5837342d3477f0
Oleg Pudeyev <oleg@bsdpower.com>
2015-12-24 04:34:51 -0500

    Apply FreeBSD patch patch-xmms_util.c

--- xmms/util.c
+++ xmms/util.c
@@ -175,7 +175,7 @@ GdkImage *create_dblsize_image(GdkImage * img)
 	 * This needs to be optimized
 	 */
 
-	dblimg = gdk_image_new(GDK_IMAGE_NORMAL, gdk_visual_get_best(), img->width << 1, img->height << 1);
+	dblimg = gdk_image_new(GDK_IMAGE_NORMAL, gdk_visual_get_best_with_depth(img->depth), img->width << 1, img->height << 1);
 	if (dblimg->bpp == 1)
 	{
 		char *srcptr, *ptr, *ptr2;
